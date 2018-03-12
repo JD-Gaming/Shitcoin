0a5976948e4a292b9b4900e8dff73202b86b2308
Daniel Lindsäth <alkanen@gmail.com>
2018-03-12 14:11:14 +0100

    Added a new modinv function that actually calculates the correct values.

--- tools/ellipticCurve.py
+++ tools/ellipticCurve.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from cipolla import cipolla
 
 
@@ -10,10 +12,10 @@ class EllipticPoint(object):
         self.y = y
 
     def __str__(self):
-        return "EllipticPoint(%d, %d)" % (self.x, self.y)
+        return "(%d, %d)" % (self.x, self.y)
 
     def __repr__(self):
-        return self.__str__()
+        return "EllipticPoint%s" % self.__str__()
 
 
 class EllipticCurve(object):
@@ -31,6 +33,23 @@ class EllipticCurve(object):
 
         # Validate?  (4 * (a ** 3) + 27 * (b **2)) % q != 0
 
+    def euclidian_gcd(self, a, b):
+        if a == 0:
+            return (b, 0, 1)
+        else:
+            g, y, x = self.euclidian_gcd(b % a, a)
+            return (g, x - (b // a) * y, y)
+
+    def modinv(self, a):
+        while a < 0:
+            a += self.q
+
+        g, x, y = self.euclidian_gcd(a, self.q)
+        if g != 1:
+            raise ValueError("No modular inverse of %d and %d exists" % (a, self.q))
+        else:
+            return x % self.q
+
     def invert(self, val):
         t = 0
         r = self.q
@@ -45,7 +64,7 @@ class EllipticCurve(object):
         if r > 1:
             raise ValueError("Unable to invert %d (%d)" % (val, r))
 
-        if t < 0:
+        while t < 0:
             t += self.q
 
         return t
@@ -94,11 +113,13 @@ class EllipticCurve(object):
             # P1 - P1 = 0
             return EllipticCurve.Zero
         if P1.x == P2.x:
-            tmp1 = 3 * pow(P1.x, 2, self.q)
-            inv = self.invert(2 * P1.y)
-            slant = ((tmp1 + self.a) * inv) % self.q
+            tmp1 = 3 * pow(P1.x, 2)
+            #inv = self.invert(2 * P1.y)
+            inv = self.modinv(2 * P1.y)
+            slant = ((tmp1 + self.a) * inv)
         else:
-            slant = ((P2.y - P1.y) * self.invert(P2.x - P1.x)) % self.q
+            #slant = ((P2.y - P1.y) * self.invert(P2.x - P1.x))
+            slant = ((P2.y - P1.y) * self.modinv(P2.x - P1.x))
 
         x = (slant**2 - P1.x - P2.x) % self.q
         y = (slant * (P1.x - x) - P1.y) % self.q
@@ -122,7 +143,7 @@ class EllipticCurve(object):
         return self.pointMultiply(self.pointAdd(P, P), n // 2)
 
 
-def main():
+def bitcoinTest():
     # Bitcoin curve
     ec = EllipticCurve(0, 7,
             0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F)  # noqa
@@ -139,6 +160,51 @@ def main():
     for i in range(1000):
         if not ec.pointValidate(ec.pointMultiply(Pp[0], 0x1 << i)):
             print("Failed at 1 << %d" % i)
+
+
+def smallTest():
+    ec = EllipticCurve(2, 2, 17)
+    P = EllipticPoint(0, 6)
+
+    img = np.ones((17, 17), dtype=np.uint8)
+
+    for i in range(1, 20):
+        pp = ec.pointMultiply(P, i)
+        print("%2d: %s" % (i, pp))
+        img[pp.y, pp.x] = 0
+
+    print(img)
+
+    expected = [
+        EllipticPoint(0, 0),
+        EllipticPoint(0, 6),
+        EllipticPoint(9, 1),
+        EllipticPoint(6, 3),
+        EllipticPoint(7, 6),
+        EllipticPoint(10, 11),
+        EllipticPoint(3, 1),
+        EllipticPoint(13, 10),
+        EllipticPoint(5, 16),
+        EllipticPoint(16, 13),
+        EllipticPoint(16, 4),
+        EllipticPoint(5, 1),
+        EllipticPoint(13, 7),
+        EllipticPoint(3, 16),
+        EllipticPoint(10, 6),
+        EllipticPoint(7, 11),
+        EllipticPoint(6, 14),
+        EllipticPoint(9, 16),
+        EllipticPoint(0, 11),
+        EllipticPoint(0, 1),
+    ]
+    pp = EllipticCurve.Zero
+    for i in range(1, 20):
+        pp = ec.pointAdd(pp, P)
+        print("%2d: %8s / %8s" % (i, pp, expected[i]))
+
+
+def main():
+    smallTest()
 
 
 if __name__ == '__main__':
