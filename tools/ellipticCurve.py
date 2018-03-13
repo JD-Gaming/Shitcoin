3e3caf9eef62e5875a1ce6ceb14157ff26b33576
Daniel Lindsäth <alkanen@gmail.com>
2018-03-13 13:40:02 +0100

    Split the elliptic curve stuff into a curve class and a point class.

--- tools/ellipticCurve.py
+++ tools/ellipticCurve.py
@@ -1,26 +1,8 @@
-import numpy as np
-
 from cipolla import cipolla
-
-
-class EllipticPoint(object):
-    def __init__(self, x, y):
-        """
-        Defines a point on an elliptic curve.
-        """
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return "(%d, %d)" % (self.x, self.y)
-
-    def __repr__(self):
-        return "EllipticPoint%s" % self.__str__()
+from ellipticPoint import *
 
 
 class EllipticCurve(object):
-    Zero = EllipticPoint(0, 0)
-
     def __init__(self, a, b, q):
         """
         Creates an elliptic curve using the definition:
@@ -30,8 +12,10 @@ class EllipticCurve(object):
         self.a = a
         self.b = b
         self.q = q
+        self.zero = EllipticPoint(0, 0, self)
 
-        # Validate?  (4 * (a ** 3) + 27 * (b **2)) % q != 0
+    def __eq__(self, other):
+        return self.a == other.a and self.b == other.b and self.q == other.q
 
     def euclidian_gcd(self, a, b):
         if a == 0:
@@ -40,57 +24,51 @@ class EllipticCurve(object):
             g, y, x = self.euclidian_gcd(b % a, a)
             return (g, x - (b // a) * y, y)
 
-    def modinv(self, a):
-        while a < 0:
-            a += self.q
+    def modInvert(self, val):
+        """
+        Calculate the value of val^-1 % q.  It is defined as the value which
+        when multiplied with val modulo q gives the value 1:
 
-        g, x, y = self.euclidian_gcd(a, self.q)
+        val * val^-1 == 1 (mod q)
+        """
+        while val < 0:
+            val += self.q
+
+        g, x, y = self.euclidian_gcd(val, self.q)
         if g != 1:
-            raise ValueError("No modular inverse of %d and %d exists" % (a, self.q))
+            raise ValueError("No modular inverse of %d and %d exists" % (
+                val, self.q))
         else:
             return x % self.q
 
-    def invert(self, val):
-        t = 0
-        r = self.q
-        new_t = 1
-        new_r = val
+    def modSqrt(self, val):
+        """
+        Calculate the square roots of val mod q, defined as the values that
+        multiplied with themselves modulo q give the result val:
 
-        while new_r != 0:
-            quotient = r // new_r
-            (t, new_t) = (new_t, t - quotient * new_t)
-            (r, new_r) = (new_r, r - quotient * new_r)
-
-        if r > 1:
-            raise ValueError("Unable to invert %d (%d)" % (val, r))
-
-        while t < 0:
-            t += self.q
-
-        return t
-
-    def sqrt(self, val):
+        x1^2 == val (mod q)
+        x2^2 == val (mod q)
+        """
         try:
             return cipolla(val, self.q)
 
         except ValueError:
             raise ValueError("Unable to find a square root of %d" % val)
 
-    def pointOrder(self, G):
-        for i in range(1, self.q + 1):
-            if self.pointMultiply(G, i) == EllipticCurve.Zero:
-                return i
-
-        raise ValueError("Unable to find the order of %s" % G)
-
     def pointsAtX(self, x):
+        """
+        Finds the two valid points at x == x.
+        """
         ysq = (pow(x, 3, self.q) + self.a * x + self.b) % self.q
-        y1, y2 = self.sqrt(ysq)
+        y1, y2 = self.modSqrt(ysq)
 
-        return EllipticPoint(x, y1), EllipticPoint(x, y2)
+        return EllipticPoint(x, y1, self), EllipticPoint(x, y2, self)
 
     def pointValidate(self, P):
-        if P == self.Zero:
+        """
+        Finds out if a point P can exist on the curve.
+        """
+        if P == self.zero:
             return True
 
         left = (P.y ** 2) % self.q
@@ -98,113 +76,10 @@ class EllipticCurve(object):
 
         return left == right
 
-    def pointNegate(self, P):
-        return EllipticPoint(P.x, -P.y % self.q)
-
-    def pointAdd(self, P1, P2):
-        """
-        Returns the sum of points P1 and P2 on the elliptic curve.
-        """
-        if P1 == EllipticCurve.Zero:
-            return P2
-        if P2 == EllipticCurve.Zero:
-            return P1
-        if P1.x == P2.x and (P1.y != P2.y or P1.y == 0):
-            # P1 - P1 = 0
-            return EllipticCurve.Zero
-        if P1.x == P2.x:
-            tmp1 = 3 * pow(P1.x, 2)
-            #inv = self.invert(2 * P1.y)
-            inv = self.modinv(2 * P1.y)
-            slant = ((tmp1 + self.a) * inv)
-        else:
-            #slant = ((P2.y - P1.y) * self.invert(P2.x - P1.x))
-            slant = ((P2.y - P1.y) * self.modinv(P2.x - P1.x))
-
-        x = (slant**2 - P1.x - P2.x) % self.q
-        y = (slant * (P1.x - x) - P1.y) % self.q
-
-        return EllipticPoint(x, y)
-
-    def pointMultiply(self, P, n):
-        """
-        Multiplies by the double-and-add algorithm.  Needs to be iterative
-        rather than recursive in order to handle 256 bit numbers.
-        """
-        if n == 0:
-            return EllipticCurve.Zero
-
-        if n == 1:
-            return P
-
-        if n % 2 == 1:
-            return self.pointAdd(P, self.pointMultiply(P, n - 1))
-
-        return self.pointMultiply(self.pointAdd(P, P), n // 2)
-
-
-def bitcoinTest():
-    # Bitcoin curve
-    ec = EllipticCurve(0, 7,
-            0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F)  # noqa
-
-    Pp = ec.pointsAtX(3)
-    print(ec.pointMultiply(Pp[0], 3).x)
-    print(ec.pointAdd(Pp[0], ec.pointAdd(Pp[0], Pp[0])).x)
-
-    Pp = ec.pointsAtX(
-        0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364146)
-    print(ec.pointMultiply(
-        Pp[0], 0x1FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC20).x)
-
-    for i in range(1000):
-        if not ec.pointValidate(ec.pointMultiply(Pp[0], 0x1 << i)):
-            print("Failed at 1 << %d" % i)
-
-
-def smallTest():
-    ec = EllipticCurve(2, 2, 17)
-    P = EllipticPoint(0, 6)
-
-    img = np.ones((17, 17), dtype=np.uint8)
-
-    for i in range(1, 20):
-        pp = ec.pointMultiply(P, i)
-        print("%2d: %s" % (i, pp))
-        img[pp.y, pp.x] = 0
-
-    print(img)
-
-    expected = [
-        EllipticPoint(0, 0),
-        EllipticPoint(0, 6),
-        EllipticPoint(9, 1),
-        EllipticPoint(6, 3),
-        EllipticPoint(7, 6),
-        EllipticPoint(10, 11),
-        EllipticPoint(3, 1),
-        EllipticPoint(13, 10),
-        EllipticPoint(5, 16),
-        EllipticPoint(16, 13),
-        EllipticPoint(16, 4),
-        EllipticPoint(5, 1),
-        EllipticPoint(13, 7),
-        EllipticPoint(3, 16),
-        EllipticPoint(10, 6),
-        EllipticPoint(7, 11),
-        EllipticPoint(6, 14),
-        EllipticPoint(9, 16),
-        EllipticPoint(0, 11),
-        EllipticPoint(0, 1),
-    ]
-    pp = EllipticCurve.Zero
-    for i in range(1, 20):
-        pp = ec.pointAdd(pp, P)
-        print("%2d: %8s / %8s" % (i, pp, expected[i]))
-
 
 def main():
-    smallTest()
+    print(("There are no tests for the elliptic curve class at the "
+           "moment, try the elliptic point class instead."))
 
 
 if __name__ == '__main__':
---
+++ tools/ellipticPoint.py
@@ -0,0 +1,126 @@
+from ellipticCurve import *
+
+
+class EllipticPoint(object):
+    def __init__(self, x, y, curve):
+        self.x = x
+        self.y = y
+        self.ec = curve
+
+    def __str__(self):
+        return "(%d, %d)" % (self.x, self.y)
+
+    def __repr__(self):
+        return "EllipticPoint%s" % self.__str__()
+
+    def __eq__(self, other):
+        """
+        Compare two points, all params must be equal for the points to be.
+        """
+        return self.x == other.x and self.y == other.y and self.ec == other.ec
+
+    def isValid(self):
+        return self.ec.pointValidate(self)
+
+    def order(self):
+        """
+        An incredibly naive function for finding the order of a generator.  It
+        simply loops through all multiples to see when the result is the zero
+        point, indicating that the generator has gone through its entire span.
+        """
+        for i in range(1, self.ec.q + 1):
+            if self.multiply(i) == self.ec.zero:
+                return i
+
+        raise ValueError("Unable to find the order of %s" % self.__str__())
+
+    def negate(self):
+        """
+        Returns the negation of the point, i.e. the mirror point over the
+        x axis:
+
+        -(x, y) == (x, -y).
+        """
+        return EllipticPoint(self.x, -self.y % self.ec.q, self.ec)
+
+    def add(self, P):
+        """
+        Returns the sum of this point and point P on the elliptic curve.
+        """
+        if self == self.ec.zero:
+            return P
+
+        if P == self.ec.zero:
+            return self
+
+        if self.x == P.x and (self.y != P.y or self.y == 0):
+            # P1 - P1 = 0
+            return self.ec.zero
+
+        if self.x == P.x:
+            tmp1 = 3 * pow(self.x, 2)
+            inv = self.ec.modInvert(2 * self.y)
+            slant = ((tmp1 + self.ec.a) * inv)
+        else:
+            slant = ((P.y - self.y) * self.ec.modInvert(P.x - self.x))
+
+        x = (slant**2 - self.x - P.x) % self.ec.q
+        y = (slant * (self.x - x) - self.y) % self.ec.q
+
+        return EllipticPoint(x, y, self.ec)
+
+    def multiply(self, n):
+        """
+        Multiplies by the double-and-add algorithm.  Needs to be iterative
+        rather than recursive in order to handle really large numbers.
+        """
+        if n == 0:
+            return self.ec.zero
+
+        if n == 1:
+            return self
+
+        if n % 2 == 1:
+            P1 = EllipticPoint(self.x, self.y, self.ec)
+            P2 = EllipticPoint(self.x, self.y, self.ec)
+            return P1.add(P2.multiply(n - 1))
+
+        return self.add(self).multiply(n // 2)
+
+
+def main():
+    ec = EllipticCurve(2, 2, 17)
+    ep = EllipticPoint(0, 6, ec)
+
+    expected = [
+        EllipticPoint(0, 0, ec),
+        EllipticPoint(0, 6, ec),
+        EllipticPoint(9, 1, ec),
+        EllipticPoint(6, 3, ec),
+        EllipticPoint(7, 6, ec),
+        EllipticPoint(10, 11, ec),
+        EllipticPoint(3, 1, ec),
+        EllipticPoint(13, 10, ec),
+        EllipticPoint(5, 16, ec),
+        EllipticPoint(16, 13, ec),
+        EllipticPoint(16, 4, ec),
+        EllipticPoint(5, 1, ec),
+        EllipticPoint(13, 7, ec),
+        EllipticPoint(3, 16, ec),
+        EllipticPoint(10, 6, ec),
+        EllipticPoint(7, 11, ec),
+        EllipticPoint(6, 14, ec),
+        EllipticPoint(9, 16, ec),
+        EllipticPoint(0, 11, ec),
+        EllipticPoint(0, 0, ec)
+    ]
+
+    pp = ec.zero
+    for i in range(0, 20):
+        print("%2d: %8s / %8s - %s" % (
+            i, pp, expected[i], "true" if (pp == expected[i]) else "false"))
+        pp = pp.add(ep)
+
+
+if __name__ == '__main__':
+    main()
